3d76b2246311764f6b1bba4d83a8be014c32924a
Bedi561 <110516777+Bedi561@users.noreply.github.com>
2024-12-10 11:18:51 +0530

    new

--- admin/src/config.js
+++ admin/src/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:3001/admin';
+// export const BASE_URL = 'http://localhost:3001/admin';
+export const BASE_URL = 'https://coursebackend-1.onrender.com/admin';
